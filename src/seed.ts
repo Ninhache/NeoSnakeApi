b93d4f4acc5ca23e5ff4ac65330e9fea03ba2f72
Ninhache <mr.carambar27@gmail.com>
2024-05-23 04:38:32 +0200

    feat: improve seeding (i really hate sequelize)

--- src/seed.ts
+++ src/seed.ts
@@ -1,9 +1,7 @@
 import { Roles } from "./db/init";
-
-const roles = [
-  { id: 1, label: "Admin" },
-  { id: 2, label: "User" },
-];
+import { CampaignMap } from "./@types/db/CampaignMap";
+import * as fs from "fs";
+import * as roles from "../seeders/roles.json";
 
 export const seedRoles = async function () {
   try {
@@ -14,16 +12,39 @@ export const seedRoles = async function () {
       return;
     }
 
+    const roles = JSON.parse(fs.readFileSync("./seeders/roles.json", "utf8"));
     const rolesUpdate = await Roles.bulkCreate(roles, {
       ignoreDuplicates: true,
     });
 
-    console.log(rolesUpdate);
     if (rolesUpdate?.length > 0) {
       console.log("Roles seeded successfully");
     } else {
       console.log("Roles already seeded");
     }
+  } catch (error) {
+    console.error("Failed to seed roles:", error);
+  }
+};
+
+export const getAllCampaign = async function () {
+  try {
+    console.log("Seeding campaigns");
+    const count = await CampaignMap.findAndCountAll();
+    if (count.count > 0) {
+      console.log("campaigns already seeded");
+      return;
+    }
+
+    const campaigns = fs.readdirSync("./seeders/campaigns");
+    campaigns.forEach(async (campaign, index) => {
+      const campaignData = fs.readFileSync(`./seeders/campaigns/${campaign}`);
+      const parsedCampaign = JSON.parse(campaignData.toString());
+      await CampaignMap.create({
+        id: index + 1,
+        map_data: parsedCampaign,
+      });
+    });
   } catch (error) {
     console.error("Failed to seed roles:", error);
   }
